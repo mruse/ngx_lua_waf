479394976e978cf71a545a71e47909817b2eb45c
MrUse <mruse@users.noreply.github.com>
2015-05-26 00:53:53 +0800

    Update install.sh
    + 新增配置方便自定义路径；
    * 修改判断及文件路径，改为单行判断，绝对路径，方便手动逐条复制执行；
    * 编译参数增加换行，提高可读性，方便修改；

--- install.sh
+++ install.sh
@@ -1,47 +1,61 @@
-mkdir -p /data/src
-cd /data/src
-if [ ! -x "LuaJIT-2.0.0.tar.gz" ]; then  
-wget http://luajit.org/download/LuaJIT-2.0.0.tar.gz
-fi
-tar zxvf LuaJIT-2.0.0.tar.gz
-cd LuaJIT-2.0.0
+# Define
+SRC="/data/src"
+PREFIX="/usr/local"
+NGINX_USER="nginx"
+NGINX_GROUP="nginx"
+NGINX_HACK="/data/logs/hack"
+
+# Check Source Dir
+[ -d "$SRC" ]||mkdir -p $SRC
+
+# Check LuaJIT TarBall
+[ -x "$SRC/LuaJIT-2.0.0.tar.gz" ]||wget http://luajit.org/download/LuaJIT-2.0.0.tar.gz -P $SRC/
+# Install LiaJIT
+tar -zxvf $SRC/LuaJIT-2.0.0.tar.gz -C $SRC/
+cd $SRC/LuaJIT-2.0.0
 make
-make install PREFIX=/usr/local/lj2
-ln -s /usr/local/lj2/lib/libluajit-5.1.so.2 /lib64/
-cd /data/src
-if [ ! -x "v0.2.17rc2.zip" ]; then  
-wget https://github.com/simpl/ngx_devel_kit/archive/v0.2.17rc2.zip
-fi
-unzip v0.2.17rc2
-if [ ! -x "v0.7.4.zip" ]; then  
-wget https://github.com/chaoslawful/lua-nginx-module/archive/v0.7.4.zip
-fi
-unzip v0.7.4
-cd /data/src
-if [ ! -x "pcre-8.10.tar.gz" ]; then
-wget http://blog.s135.com/soft/linux/nginx_php/pcre/pcre-8.10.tar.gz
-fi
-tar zxvf pcre-8.10.tar.gz
-cd pcre-8.10/
+make install PREFIX=$PREFIX/lj2
+ln -s $PREFIX/lj2/lib/libluajit-5.1.so.2 /lib64/
+
+# Install 
+[ -f "$SRC/v0.2.17rc2.zip" ]||wget https://github.com/simpl/ngx_devel_kit/archive/v0.2.17rc2.zip -P $SRC/
+unzip $SRC/v0.2.17rc2.zip -d $SRC/
+
+[ -f "$SRC/v0.7.4.zip" ]||wget https://github.com/chaoslawful/lua-nginx-module/archive/v0.7.4.zip -P $SRC/
+unzip $SRC/v0.7.4.zip -d $SRC/
+
+[ -f "$SRC/pcre-8.10.tar.gz" ]||wget http://blog.s135.com/soft/linux/nginx_php/pcre/pcre-8.10.tar.gz -P $SRC/
+tar -zxvf $SRC/pcre-8.10.tar.gz -d $SRC/
+cd $SRC/pcre-8.10/
 ./configure
 make && make install
-cd ..
-if [ ! -x "nginx-1.2.4.tar.gz" ]; then
-wget 'http://nginx.org/download/nginx-1.2.4.tar.gz'
-fi
-tar -xzvf nginx-1.2.4.tar.gz
-cd nginx-1.2.4/
+
+[ -f "$SRC/nginx-1.2.4.tar.gz" ]||wget http://nginx.org/download/nginx-1.2.4.tar.gz -P $SRC/
+tar -xzvf $SRC/nginx-1.2.4.tar.gz -d $SRC/
+cd $SRC/nginx-1.2.4/
 export LUAJIT_LIB=/usr/local/lj2/lib/
 export LUAJIT_INC=/usr/local/lj2/include/luajit-2.0/
-./configure --user=daemon --group=daemon --prefix=/usr/local/nginx/ --with-http_stub_status_module --with-http_sub_module --with-http_gzip_static_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module  --add-module=../ngx_devel_kit-0.2.17rc2/ --add-module=../lua-nginx-module-0.7.4/
+./configure \
+--user=$NGINX_USER \
+--group=$NGINX_GROUP \
+--prefix=$PREFIX/nginx/ \
+--with-http_stub_status_module \
+--with-http_sub_module \
+--with-http_gzip_static_module \
+--without-mail_pop3_module \
+--without-mail_imap_module \
+--without-mail_smtp_module  \
+--add-module=$SRC/ngx_devel_kit-0.2.17rc2/ \
+--add-module=$SRC/lua-nginx-module-0.7.4/
 make -j8
 make install 
-#rm -rf /data/src
-cd /usr/local/nginx/conf/
+
+cd $PREFIX/nginx/conf/
 wget https://github.com/loveshell/ngx_lua_waf/archive/master.zip --no-check-certificate
 unzip master.zip
-mv ngx_lua_waf-master/* /usr/local/nginx/conf/
+mv ngx_lua_waf-master/* $PREFIX/nginx/conf/
+[ -d "" ]||mkdir -p $NGINX_HACK
+chmod -R 775 $NGINX_HACK
+
 rm -rf ngx_lua_waf-master
-rm -rf /data/src
-mkdir -p /data/logs/hack
-chmod -R 775 /data/logs/hack
+rm -rf $SRC/*
